0bd0f2a4f67b9121575978a2811edea8d231732f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-05-16 14:54:52 +0900

    :bulb: フォルダ移動

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@
 var electron = require("electron");
 var app = electron.app;
 var remote = electron.remote;
-var application = require("./app/application");
+var application = require("./scripts/application");
 application.initialize();
